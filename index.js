752ede04fdc441187650928e4e6d9fc8303418e1
Rob <morrisrjc@gmail.com>
2021-01-16 00:01:10 +0000

    removed some notes

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
